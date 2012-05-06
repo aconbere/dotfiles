ff6344fc1e29ec43ecbc6f3e63fa9713e6925108
Anders Conbere <aconbere@gmail.com>
2012-05-06 11:03:14 -0500

    adding in dzen config to xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,25 +1,23 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
---import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
---main = do
---  xmonad $ defaultConfig {
---         terminal = "urxvt"
---         }
 main = do
     xmproc <- spawnPipe "/home/aconbere/.cabal/bin/xmobar /home/aconbere/.xmobarrc"
-    xmonad $ defaultConfig {
-          terminal = "urxvt"
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        } `additionalKeys` [
-          ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        ]
+    xmonad 
+        $ withUrgencyHook dzenUrgencyHook { args = ["-bg", "darkgreen", "-xs", "1"] }
+        $ defaultConfig {
+              terminal = "urxvt"
+            , manageHook = manageDocks <+> manageHook defaultConfig
+            , layoutHook = avoidStruts  $  layoutHook defaultConfig
+            , logHook = dynamicLogWithPP xmobarPP
+                            { ppOutput = hPutStrLn xmproc
+                            , ppTitle = xmobarColor "green" "" . shorten 50
+                            }
+            } `additionalKeys` [
+              ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+            ]
